acf084d68f9a29e7333958dbdbcf361de7f8d0ae
Francisco Riveros <francisco@Franciscos-MacBook-Air.local>
2023-01-22 21:04:40 -0300

    iniciando el modulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+nombre:string='Francisco riveors ';
+
+mostrarNombre(){
+  console.log(this.nombre);
+  
+}
 }
